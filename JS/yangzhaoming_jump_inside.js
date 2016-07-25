87336e42d043800e5e8618bbcea4666ab87a1ac8
HiGoodToSeeUAgain <HiGoodToSeeUAgain>
2016-07-25 15:39:19 +0800

    add test

---
+++ JS/yangzhaoming_jump_inside.js
@@ -0,0 +1,35 @@
+// JavaScript Document 版权 @照明·楊 所有 即使本人同意了，也不能随意转载^_^:)
+$(document).ready(function(){
+   $(".un1").click(function(){
+      $("html,body").animate({scrollTop:$("#s1").offset().top},1000);
+    });
+	$(".co2").click(function(){
+      $("html,body").animate({scrollTop:$("#s2").offset().top},1000);
+    });
+
+	$(".gr3").click(function(){
+		  $("html,body").animate({scrollTop:$("#s3").offset().top},1000);
+		});
+	
+	$(".dr4").click(function(){
+		  $("html,body").animate({scrollTop:$("#s4").offset().top},1000);
+		});
+	
+	$(".mo5").click(function(){
+		  $("html,body").animate({scrollTop:$("#s5").offset().top},1000);
+		});
+	
+	$(".mu6").click(function(){
+		  $("html,body").animate({scrollTop:$("#s6").offset().top},1000);
+		});
+	
+	$(".fl7").click(function(){
+		  $("html,body").animate({scrollTop:$("#s7").offset().top},1000);
+		});
+	
+	$(".ho8").click(function(){
+		  $("html,body").animate({scrollTop:$("#s8").offset().top},1000);
+		});
+
+})
+
